03b6afb836bdafb204e8270a93f7a13405c5aade
angygbg <angie02089@gmail.com>
2023-05-30 20:42:30 -0600

    txtarea

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const salida = document.querySelector(".txt-salida");
 const copy = document.querySelector(".btn-copiar");
 const oculto = document.querySelector(".ocultar");
 copy.style.display= "none";
-oculto.style.display = ""
+oculto.style.display = "";
+salida.style.display = "none";
 
 function validarTxt(){
     let textoEs = document.querySelector(".text-ingres").value;
